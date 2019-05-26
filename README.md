3d4b45b51e06fb0f8fde7ada902dcfe5481b26bf
Rob Richards <rtfminc@gmail.com>
2019-05-25 21:09:32 -0700

    Updated and added basic Binance functions.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # cryptobot
+Currently this will have functions that will utilize Binance exchange. The Bots are coming.
+Binance specific methods.
+
+## File: binancefunctions.py
+* def authenticate() - authentication to Binance. API Keys are stored as environment vars.
+* def get_token_list() - Will get each token and its latest price. 
+* def binance_price(number=42) - Will currnetly return EOSTUSD. Returns 42, 88, or user set # of rows.
+* def clean_dates(df_dates) - leans the dates that Binance returns.
+* def get_x_info(token_symbol) - Given a symbol (like EOS), get the associated assets on Binance.
--- binancefunctions.py
+++ binancefunctions.py
@@ -1,23 +1,40 @@
 import os
+import pandas as pd
 from binance.client import Client
 from datetime import datetime
 from pandas import DataFrame as df
 
-def get_token_list():
-    # Will get each token and its latest price. 
-    # Returns a DataFrame 
-    client = Client(api_key=os.environ.get('BINANCE_KEY'), api_secret=os.environ.get('BINANCE_SECRET'))
+# Binance specific methods.
+# - def authenticate():
+# - def get_token_list():
+# - def binance_price(number=42):
+# - def clean_dates(df_dates):
+# - def get_x_info(token_symbol):
+
+# Authenticate to Binance.
+def authenticate():
+    print ("Authenticating...")
+    return Client(api_key=os.environ.get('BINANCE_KEY'), api_secret=os.environ.get('BINANCE_SECRET'))
+
+
+# Will get each token and its latest price. 
+def get_token_list(): 
+    client = authenticate()
     tokens = client.get_all_tickers()
     tokens_df = df(tokens)
     return tokens_df
 
 
-def binance_price():
-    # Get some EOS data - comes back as a list of lists. This is raw looking data.
-    # The docs show we have date/time, open, high, low, close price, volume, ...
-    # This will get a list of 42 items.
-    client = Client(api_key=os.environ.get('BINANCE_KEY'), api_secret=os.environ.get('BINANCE_SECRET'))
-    candles = client.get_klines(symbol='EOSTUSD', interval=Client.KLINE_INTERVAL_1HOUR, limit=42) 
+# Get some EOS data - comes back as a list of lists. This is raw looking data.
+# The docs show we have date/time, open, high, low, close price, volume, ...
+# This will get a default list of 42 items, unless a non int is passed, then 88.
+# Returns a DataFrame.
+def binance_price(number=42):
+    client = authenticate()
+    if not isinstance(number, int):
+        number = 88
+    print ("Getting ", number, " EOSTUSD hourly prices...", sep="")
+    candles = client.get_klines(symbol='EOSTUSD', interval=Client.KLINE_INTERVAL_1HOUR, limit=number) 
 
     # Let's make it look nicer, a dataframe! Returns 11 columns and 42 rows (500 is default/max)
     candles_df = df(candles)
@@ -36,13 +53,13 @@ def binance_price():
     return final_df
 
 
+# Cleans the dates that Binance returns.
+# Binance uncleaned date data looks like: "1558562400000"
 def clean_dates(df_dates):
     dates = []
-
-    # Convert the date data "number" to a date we can work with. Remove milliseconds by division.
-    # In this case the uncleaned data looks like: "1558562400000"
+ 
     for time in df_dates:
-        fixed = datetime.fromtimestamp(int(time/1000))
+        fixed = datetime.fromtimestamp(int(time/1000))   # Remove milliseconds by division.
         dates.append(fixed)
 
     # Let's convert it to a DataFrame and change the column name.
@@ -51,8 +68,21 @@ def clean_dates(df_dates):
     return df_fixed_date  
 
 
+# Given a symbol, get the associated assets on Binance.
+def get_x_info(token_symbol):
+    client = authenticate()
+    print ("Getting Binanaces tokens using ", token_symbol, ".", sep="")
+    tokens = client.get_exchange_info()
+    tokens_df= pd.DataFrame(tokens['symbols'])
+    tokens_df= tokens_df[['symbol', 'baseAsset',  'quoteAsset']]
+    tokens_df.set_index('symbol', inplace=True)     # "inplace" is used to remove index column of just numbers
+    tokens_df= tokens_df[tokens_df['baseAsset'] == token_symbol]
+    tokens_df.set_index('baseAsset')
+    return tokens_df
 
 
 # Test the methods.......
-# print (binance_price())  
+# print (binance_price("test"))  
 # print(get_token_list())
+# print ("Lets go")
+# print ( get_x_info('EOS') )
