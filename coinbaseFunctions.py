6f5c886826ca48ee39196e342f2efa97664aa831
Rob Richards <rtfminc@gmail.com>
2019-06-24 15:09:30 -0700

    Added coinbase prices for buy, sell, and spot..

--- coinbaseFunctions.py
+++ coinbaseFunctions.py
@@ -1,6 +1,7 @@
 import os
 from coinbase.wallet.client import Client
 import requests
+import json
 from pprint import pprint
 
 # Authenticate to Coinbase.
@@ -10,13 +11,32 @@ def authenticate():
 
 
 # Retrieve the spot price of token in USD
+# Doesn't need authentication
 def get_token_price(token="EOS"):
     response = requests.get("https://api.coinbase.com/v2/prices/"+token+"-USD/spot")
     data = response.json()
     token = data["data"]["base"]
     price = data["data"]["amount"]
     return (f"Price: {token} {price} USD")
-    
+   
+
+# Get buy, sell, and spot prices  
+# Use authenticate so we don't need to call 3 endpoints 
+def get_token_prices(token="EOS"):
+	d = dict();
+	client=authenticate()
+	d['token']= token
+	d['buy']  = client.get_buy_price(currency_pair = 'EOS-USD')['amount']
+	d['sell'] = client.get_sell_price(currency_pair = 'EOS-USD')['amount']
+	d['spot'] = client.get_spot_price(currency_pair = 'EOS-USD')['amount']
+	return d
+
+
+def get_currencies():
+	# No need for auth, use endpoints.
+	response = requests.get("https://api.coinbase.com/v2/currencies")
+	return response.json()
+
 
 def get_account_info(gettall=False):
 	total = 0
@@ -33,5 +53,16 @@ def get_account_info(gettall=False):
 print ("Let's roll...")
 # pprint (get_account_info(True))
 # print (get_token_price())
-print (get_token_price("XR"))
+# print (get_token_price("XR"))
+# pprint (get_currencies())
 
+print (get_token_prices())
+
+
+# pa=client.get_primary_account()
+# print ("\nPrimary Account:", pa)
+
+# user = client.get_current_user()
+# print ("\nCurrent user:", user)
+# print ("\nCurrent user json: ", json.dumps(user))
+# account.get_transactions()
